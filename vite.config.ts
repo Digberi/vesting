d99dcf64a2327f35761344195f3683abc4c00f0a
Digberi <orkepuma@gmail.com>
2023-06-24 16:43:09 +0300

    added transformMixedEsModules

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,12 @@ export default defineConfig({
     }),
   ],
 
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  },
+
   resolve: {
     alias: [
       { find: '@api', replacement: path.resolve(__dirname, 'src/api') },
